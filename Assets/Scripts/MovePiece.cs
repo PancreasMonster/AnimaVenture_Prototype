4f41a2127c19ebed6fff1b3cab9d8476c9bf31b0
Jack Burleigh <jrwburleigh@gmail.com>
2018-10-16 16:27:42 +0100

    Jigsaw added

---
+++ Assets/Scripts/MovePiece.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePiece : MonoBehaviour {
+    public bool pickedup = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(pickedup == true)
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        pickedup = true;
+    }
+
+    private void OnMouseUp()
+    {
+        pickedup = false;
+    }
+}
