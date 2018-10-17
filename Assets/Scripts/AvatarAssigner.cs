b602232bef5c77682ae200b84e166a6d179f2cdb
Ryan <ryanflood444@gmail.com>
2018-10-17 19:49:03 +0100

    Small Avatar Update

--- Assets/Scripts/AvatarAssigner.cs
+++ Assets/Scripts/AvatarAssigner.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class AvatarAssigner : MonoBehaviour {
 
-    public GameObject draggable; 
+    public GameObject draggable;
+    private Transform avatar;
 
 	// Use this for initialization
 	void Start () {
-        GameObject.FindGameObjectWithTag("CellManager").GetComponent<Transform>().parent = draggable.transform;
-        GameObject.FindGameObjectWithTag("CellManager").GetComponent<Transform>().localPosition = new Vector3(0, 0, 0);
-        GameObject.FindGameObjectWithTag("CellManager").GetComponent<Transform>().rotation = Quaternion.Euler(90, 180, 0);
-        GameObject.FindGameObjectWithTag("CellManager").GetComponent<Transform>().localScale = new Vector3(.2f, .2f, .2f);
+        avatar = GameObject.FindGameObjectWithTag("CellManager").GetComponent<Transform>();
+        avatar.parent = draggable.transform;
+        avatar.localPosition = new Vector3(0, 0, 0);
+        avatar.rotation = Quaternion.Euler(90, 180, 0);
+        avatar.localScale = new Vector3(.2f, .2f, .2f);
     }
 	
 	// Update is called once per frame
