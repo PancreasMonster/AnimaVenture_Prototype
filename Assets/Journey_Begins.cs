2e9ba8b52797e3cf73f9a127242ff517d10a13d9
RMBehan <C16424252@mydit.ie>
2018-10-17 01:16:15 +0100

    Added the Joureny Scene
    Added in the journey scene to project and created co-routine to stop system after fixed time and play journey audio clip

---
+++ Assets/Journey_Begins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Journey_Begins : MonoBehaviour {
+
+    public float timer;
+    public AudioSource myAudio;
+    public string textToShow;
+    public Text textBox;
+    public GameObject ParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("BeginJourney");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator BeginJourney()
+    {
+        yield return new WaitForSeconds(timer);
+        textBox.text = textToShow;
+        yield return new WaitForSeconds(timer);
+        textBox.text = "";
+        ParticleSystem.SetActive(false);
+        myAudio.Play();
+    }
+}
---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour {
+
+    public float Distance = 10; 
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton (0))
+        {
+
+
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 pos = r.GetPoint(Distance);
+
+            transform.position = pos;
+        }
+	}
+}
