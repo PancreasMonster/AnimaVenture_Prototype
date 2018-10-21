2f6c68f37b4967ff8cea4d2e3023e9a1eb6fa63f
Ryan <ryanflood444@gmail.com>
2018-10-21 15:38:21 +0100

    Journey Music

---
+++ Assets/Scripts/Audio/BGMusicRemover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicRemover : MonoBehaviour {
+
+    private GameObject BGM;
+
+	// Use this for initialization
+	void Start () {
+        BGM = GameObject.FindGameObjectWithTag("BG Music");
+        Destroy(BGM);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
