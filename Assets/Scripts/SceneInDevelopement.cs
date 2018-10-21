0f519b74f24bd97f01c82375e682f2174cfb75e2
RMBehan <C16424252@mydit.ie>
2018-10-21 17:50:37 +0100

    Implementation
    Put in the art created for the location markers and bios. Linked up the new grange art bio and implemented all markers on map. Included a message which appears when you hover over location not yet developed. Updated the UI buttons to newer version.

---
+++ Assets/Scripts/SceneInDevelopement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneInDevelopement : MonoBehaviour {
+    public Text textBox;
+    public string textToShow;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        textBox.text = textToShow;
+    }
+
+    void OnMouseExit()
+    {
+        textBox.text = "";
+    }
+}
