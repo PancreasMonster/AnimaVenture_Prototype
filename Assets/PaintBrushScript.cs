bd95d1a160e379c244a5974dd8bbd2f8b18c825c
Jack Burleigh <jrwburleigh@gmail.com>
2018-11-05 10:51:33 +0000

    Drawing Submission Scene

---
+++ Assets/PaintBrushScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBrushScript : MonoBehaviour {
+
+    public GameObject paint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(paint);
+        }
+
+        }
+
+    
+}
---
+++ Assets/PaintScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintScript : MonoBehaviour {
+
+    public GameObject colour;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
