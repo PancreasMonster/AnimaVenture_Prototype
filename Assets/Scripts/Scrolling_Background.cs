699f03a8fe3ad572ea9435874754c2e59d6a7853
PancreasMonster <ryanflood444@gmail.com>
2018-11-07 12:39:51 +0000

    Scrolling Background

--- Assets/Scripts/Scrolling_Background.cs
+++ Assets/Scripts/Scrolling_Background.cs
@@ -8,26 +8,13 @@ public class Scrolling_Background : MonoBehaviour {
     [Range(1f, 20f)]
     public float scrollSpeed = 1f;
 
-    //Scroll Offset Value to smoothly repeat background movement
-    public float scrollOffset;
-
-    //start position of background movement
-    Vector2 startPos;
-
-    //backgrounds new position
-    float newPos;
-
-	// Use this for initialization
-	void Start () {
-        //getting background's start position
-        startPos = transform.position;
-	}
+    
 	
 	// Update is called once per frame
 	void Update () {
-        //calculating new backgrounds position repeating it depending on scroll Offset
-        newPos = Mathf.Repeat(Time.time * -scrollSpeed, scrollOffset);
-        //Setting new Position
-        transform.position = startPos + Vector2.right * newPos;
+		transform.Translate (Vector3.left * scrollSpeed * Time.deltaTime);
+		if (transform.position.x <= -11.6) {
+			transform.Translate (Vector3.right * 11.6f * 2);
+		}
 	}
 }
