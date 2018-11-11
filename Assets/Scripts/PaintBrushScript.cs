f8d66ff6321d0eb52799ff3ee97eb7d8b263c0a2
Jack Burleigh <jrwburleigh@gmail.com>
2018-11-11 21:32:02 +0000

    Drawing Scene Lines Updated and save pic started

--- Assets/Scripts/PaintBrushScript.cs
+++ Assets/Scripts/PaintBrushScript.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PaintBrushScript : MonoBehaviour {
+public class PaintBrushScript : MonoBehaviour
+{
 
     public GameObject paint;
     public Color LRColour;
     private float LRHue;
     public Slider slider1;
-    int n = 0;
+
     Vector3 objPosition;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
         objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objPosition;
@@ -30,30 +33,14 @@ public class PaintBrushScript : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(paint);
-            StartCoroutine(Paint());
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            StopAllCoroutines();
-            n = 0;
         }
         
 
+
+
+
+
     }
-
-    IEnumerator Paint()
-    {
-        paint.GetComponent<LineRenderer>().SetPosition(n, objPosition);
-        paint.GetComponent<LineRenderer>().positionCount += 1;
-        n += 1;
-        yield return new WaitForSeconds(0.01f);
-        StartCoroutine(Paint());
-    }
-
-   
-
-    
-
 }
 
 
--- Assets/Scripts/PaintScript.cs
+++ Assets/Scripts/PaintScript.cs
@@ -8,21 +8,55 @@ public class PaintScript : MonoBehaviour {
     public GameObject colour;
     public LineRenderer rend;
     public Color LRcolour;
+    public Vector3 objPosition;
+    int n = 0;
+    private bool start;
 
 	// Use this for initialization
 	void Start () {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objPosition;
+        start = true;
+
+        rend.positionCount = 0;
 
         rend = GetComponent<LineRenderer>();
 
-        rend.SetPosition(0, objPosition);
-        
+        LRcolour = colour.GetComponent<Image>().color;
+
+        rend.GetComponent<LineRenderer>().SetColors(LRcolour, LRcolour);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        LRcolour = colour.GetComponent<Image>().color;
+
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+
+        
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(Paint());
+            }
+
+
+            else if (Input.GetMouseButtonUp(0))
+            {
+                StopAllCoroutines();
+                n = 0;
+                start = false;
+            }
+        
+        
+
     }
-}
+
+    IEnumerator Paint()
+    {
+        this.gameObject.GetComponent<LineRenderer>().positionCount += 1;
+        this.gameObject.GetComponent<LineRenderer>().SetPosition(n, objPosition);
+        n += 1;
+        yield return new WaitForSeconds(0.001f);
+        StartCoroutine(Paint());
+    }
+    }
+
