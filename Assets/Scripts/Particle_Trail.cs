ba0a7333f7ef18e4b3b812833c6dd2d911032d39
Patrick <C16452312@mydit.ie>
2018-11-12 02:05:08 +0000

    Journey Scene Changes and UI add

--- Assets/Scripts/Particle_Trail.cs
+++ Assets/Scripts/Particle_Trail.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particle_Trail : MonoBehaviour {
+public class Particle_Trail : MonoBehaviour
+{
 
     private TrailRenderer trail;
     public float Distance;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         trail = GetComponent<TrailRenderer>();
         //Cursor.visible = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButton(1)) 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
         {
             trail.startColor = Color.red;
             trail.endWidth = 5;
         }
 
-        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 pos = r.GetPoint(Distance);
-
-        transform.position = pos;
+        if (Input.GetMouseButton(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 pos = r.GetPoint(Distance);
+            transform.position = pos;
+        }
     }
 }
