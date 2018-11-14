cc08b6a41007d872c7848fdeab2ee136457f19f3
Patrick <C16452312@mydit.ie>
2018-11-14 23:06:50 +0000

    Trying to Make Click Particle Script

---
+++ Assets/Particle_Explode_Click.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_Explode_Click : MonoBehaviour {
+
+    public GameObject StarParticles;
+    private Vector3 mouseOrigin;
+
+    // Use this for initialization
+    void Start () {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        mouseOrigin = Input.mousePosition;
+        Destroy(this.gameObject);
+        Instantiate(StarParticles);
+    }
+}
