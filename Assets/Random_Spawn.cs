753a7b11d28106fe7dce33f6c23b8b9b2ffade8e
Patrick <C16452312@mydit.ie>
2018-11-21 14:07:21 +0000

    Trying to make a random spawn script

---
+++ Assets/Random_Spawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Spawn : MonoBehaviour
+{
+
+    private Vector3 Min;
+    private Vector3 Max;
+    private float _xAxis;
+    private float _yAxis;
+    private Vector3 _randomPosition;
+
+    void Start()
+    {
+        SetRanges();
+    }
+    void Update()
+    {
+        _xAxis = Random.Range(Min.x, Max.x);
+        _yAxis = Random.Range(Min.y, Max.y);
+        _randomPosition = new Vector3(_xAxis, _yAxis);
+    }
+    //Here put the ranges where your object will appear, or put it in the inspector.
+    void SetRanges()
+    {
+        Min = new Vector3(2, 4); //Random value.
+        Max = new Vector3(20, 40); //Another ramdon value, just for the example.
+    }
+    void InstantiateRandomObjects()
+    {
+            Instantiate(gameObject, _randomPosition, Quaternion.identity);
+    }
+}
