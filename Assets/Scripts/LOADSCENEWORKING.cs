83ea03b7ec217e92b6ebe2ffae5b273e506e8084
RMBehan <C16424252@mydit.ie>
2018-12-11 17:53:15 +0000

    Added instruction scene plus more sprites

---
+++ Assets/Scripts/LOADSCENEWORKING.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class LOADSCENEWORKING : MonoBehaviour {
+
+    public void LoadTheNextScene ( int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
